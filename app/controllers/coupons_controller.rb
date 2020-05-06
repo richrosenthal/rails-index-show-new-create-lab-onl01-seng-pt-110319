e9c8dd013ed5f40a68533b29959dec3b4f757f74
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-05-06 00:45:14 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,19 +4,20 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
   end
 
-  def show 
-    @coupon = Coupon.find(params[:id])
-  end
-
-  def new
-    
-  end
-
    def create
         coupon = Coupon.create(coupon_code: params[:coupon_code],store: params[:store])
         coupon.save
         redirect_to coupon_path(coupon)
     end
+
+  def new
+    
+  end
+
+ def show 
+    @coupon = Coupon.find(params[:id])
+  end
+ 
 
 
   
