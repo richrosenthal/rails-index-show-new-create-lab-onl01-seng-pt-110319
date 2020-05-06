733ddbcbf00494d42715b5c50ad5fd544ab7f6f2
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-05-06 00:36:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :coupons 
+  resources :coupons, only: [:index, :new, :create]
+  get '/coupon/:id', to: 'coupons#show', as: 'coupon' 
 
   
 end
